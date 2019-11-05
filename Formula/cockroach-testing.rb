eb6c468f936854a4555075b659782d74b78feec1
David López <not4rent@gmail.com>
2019-11-05 18:00:04 +0100

    fix testing formula

---
+++ Formula/cockroach-testing.rb
@@ -0,0 +1,101 @@
+class CockroachTesting < Formula
+  desc "Distributed SQL database"
+  homepage "https://www.cockroachlabs.com"
+  url "https://binaries.cockroachdb.com/cockroach-v19.2.0-rc.3.darwin-10.9-amd64.tgz"
+  version "19.2.0-rc3"
+  sha256 "6257696dfd57e8c9a47853942ea4b307079996806bf4f1e7e02766b5b8be8fa1"
+  # This revision facilitates conversion from homebrew-core to the tap.
+  # TODO(bdarnell): Remove this once we've updated to a version that never appeared in homebrew-core.
+  revision 1
+
+  bottle :unneeded
+
+  def install
+    bin.install "cockroach"
+    system "#{bin}/cockroach", "gen", "man", "--path=#{man1}"
+
+    bash_completion.mkpath
+    system "#{bin}/cockroach", "gen", "autocomplete", "bash", "--out=#{bash_completion}/cockroach"
+
+    zsh_completion.mkpath
+    system "#{bin}/cockroach", "gen", "autocomplete", "zsh", "--out=#{zsh_completion}/_cockroach"
+  end
+
+  def caveats; <<~EOS
+    For local development only, this formula ships a launchd configuration to
+    start a single-node cluster that stores its data under:
+      #{var}/cockroach/
+    Instead of the default port of 8080, the node serves its admin UI at:
+      #{Formatter.url("http://localhost:26256")}
+
+    Do NOT use this cluster to store data you care about; it runs in insecure
+    mode and may expose data publicly in e.g. a DNS rebinding attack. To run
+    CockroachDB securely, please see:
+      #{Formatter.url("https://www.cockroachlabs.com/docs/secure-a-cluster.html")}
+  EOS
+  end
+
+  plist_options :manual => "cockroach start --insecure"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/cockroach</string>
+        <string>start</string>
+        <string>--store=#{var}/cockroach/</string>
+        <string>--http-port=26256</string>
+        <string>--insecure</string>
+        <string>--host=localhost</string>
+      </array>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    begin
+      # Redirect stdout and stderr to a file, or else  `brew test --verbose`
+      # will hang forever as it waits for stdout and stderr to close.
+      # TODO(bdarnell): this mkfifo will be unnecessary in 19.2 (#39300)
+      system "mkfifo", "listen_url_fifo"
+      system "#{bin}/cockroach start --insecure --background --listen-addr=127.0.0.1:0 --http-addr=127.0.0.1:0 --listening-url-file=listen_url_fifo&> start.out"
+      # TODO(bdarnell): remove the X from this variable and the --url flags after
+      # https://github.com/cockroachdb/cockroach/issues/40747 is fixed.
+      ENV["XCOCKROACH_URL"] = File.read("listen_url_fifo").strip
+      pipe_output("#{bin}/cockroach sql --url=$XCOCKROACH_URL", <<~EOS)
+        CREATE DATABASE bank;
+        CREATE TABLE bank.accounts (id INT PRIMARY KEY, balance DECIMAL);
+        INSERT INTO bank.accounts VALUES (1, 1000.50);
+      EOS
+      output = pipe_output("#{bin}/cockroach sql --url=$XCOCKROACH_URL --format=csv",
+        "SELECT * FROM bank.accounts;")
+      assert_equal <<~EOS, output
+        id,balance
+        1,1000.50
+      EOS
+    rescue => e
+      # If an error occurs, attempt to print out any messages from the
+      # server.
+      begin
+        $stderr.puts "server messages:", File.read("start.out")
+      rescue
+        $stderr.puts "unable to load messages from start.out"
+      end
+      raise e
+    ensure
+      system "#{bin}/cockroach", "quit", "--url=$XCOCKROACH_URL"
+    end
+  end
+end
